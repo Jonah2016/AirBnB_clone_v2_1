f8892a6781d46c3a54d05786429ff3bd15136922
“Jonah <etuafuljonah@gmail.com>
2024-01-14 15:34:37 -0800

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,65 +1,47 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric import task, Connection, Config
+""" a Fabric script (based on the file 1-pack_web_static.py) that distributes..
+    ..an archive to your web servers, using the function do_deploy: """
 
-env = {"hosts": ["54.165.43.217", "34.229.70.47"]}
 
-@task
-def do_deploy(ctx, archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
+from fabric.api import *
+from datetime import datetime
+from os.path import exists
+
+
+env.hosts = ["54.165.43.217", "34.229.70.47"]  # <IP web-01>, <IP web-02>
+# ^ All remote commands must be executed on your both web servers
+# (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+
+
+def do_deploy(archive_path):
+    """ distributes an archive to my web servers
     """
-    if os.path.isfile(archive_path) is False:
+    if exists(archive_path) is False:
+        return False  # Returns False if the file at archive_path doesnt exist
+    filename = archive_path.split('/')[-1]
+    # so now filename is <web_static_2021041409349.tgz>
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    # curr = '/data/web_static/current'
+    tmp = "/tmp/" + filename
+
+    try:
+        put(archive_path, "/tmp/")
+        # ^ Upload the archive to the /tmp/ directory of the web server
+        run("mkdir -p {}/".format(no_tgz))
+        # Uncompress the archive to the folder /data/web_static/releases/
+        # <archive filename without extension> on the web server
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        # ^ Delete the archive from the web server
+        run("rm -rf /data/web_static/current")
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        # Create a new the symbolic link /data/web_static/current on the
+        # web server, linked to the new version of your code
+        # (/data/web_static/releases/<archive filename without extension>)
+        return True
+    except:
         return False
-
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
-    # Create a Connection object
-    conn = Connection(ctx["host"])
-
-    # Use the Connection object to run commands
-    result = conn.put(archive_path, "/tmp/{}".format(file))
-    if result.failed:
-        return False
-
-    result = conn.run("rm -rf /data/web_static/releases/{}/".format(name))
-    if result.failed:
-        return False
-
-    result = conn.run("mkdir -p /data/web_static/releases/{}/".format(name))
-    if result.failed:
-        return False
-
-    result = conn.run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
-    if result.failed:
-        return False
-
-    result = conn.run("rm /tmp/{}".format(file))
-    if result.failed:
-        return False
-
-    result = conn.run("mv /data/web_static/releases/{}/web_static/* "
-                      "/data/web_static/releases/{}/".format(name, name))
-    if result.failed:
-        return False
-
-    result = conn.run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-    if result.failed:
-        return False
-
-    result = conn.run("rm -rf /data/web_static/current")
-    if result.failed:
-        return False
-
-    result = conn.run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
-    if result.failed:
-        return False
-
-    return True
 
